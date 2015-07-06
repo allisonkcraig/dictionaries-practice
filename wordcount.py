cee77b97ff48bdcbdcc72be4f5033ef30a369e63
Hackbright Student <info@hackbrightacademy.com>
2015-07-06 15:54:38 -0700

    working counter

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,21 @@
 # put your code here.
+
+opened_file = open('test.txt')
+
+poem_words = {}
+
+words = []
+
+for line in opened_file:
+    words = line.split( )
+    for word in words:
+        if word not in poem_words:
+            poem_words[word] = 1
+        else:
+            poem_words[word] += 1
+# if word != poem_words[word]:
+
+
+print poem_words
+
+
