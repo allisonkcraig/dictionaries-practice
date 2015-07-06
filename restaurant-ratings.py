def7a3300981d5041edd68aac43ae696f02ec53c
Hackbright Student <info@hackbrightacademy.com>
2015-07-06 16:19:56 -0700

    added restaurant ratings file

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,1 +1,10 @@
-# your code goes here
+opened_file = open('scores.txt')
+
+restaurant = {}
+
+for line in opened_file:
+    new_line = line.rstrip().split(":")
+    restaurant[new_line[0]] = new_line[1]
+
+for restaurant_name, rating in restaurant.items():
+    print "%s is rated at %s" % (restaurant_name, rating)
